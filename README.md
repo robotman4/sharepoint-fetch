356298ad347f085c68c7fe5b1d0eadb5f09b2c85
felipgit <kim@felip.se>
2024-10-24 23:04:46 +0200

    first poc

---
+++ README.md
@@ -0,0 +1,10 @@
+### Azure
+1. Create an app registration in your Microsoft 365 tenant.
+2. Give it following permissions:
+    - Files.Read.All (Application)
+    - Sites.Read.All (Application)
+3. Grant admin consent.
+4. Create a secret.
+
+### Script
+Change the values in the start of the script and run it.
---
+++ download.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+client_id="YOUR-CLIENT-ID-GOES-HERE"
+client_secret="YOUR-APP-SECRET-VALUE-GOES-HERE"
+tenant_id="YOUR-TENANT-ID-GOES-HERE"
+site_name="MySite" # The name of your site
+sharepoint_url="example.sharepoint.com"
+file_name="filename.txt"
+destination="/tmp/" # Where to save the file
+
+echo Create login
+response_access=$(curl -s -X POST \
+    "https://login.microsoftonline.com/$tenant_id/oauth2/v2.0/token" \
+    -F grant_type=client_credentials \
+    -F client_id=$client_id \
+    -F client_secret=$client_secret \
+    -F scope=https://graph.microsoft.com/.default)
+
+access_token=$(echo $response_access | jq -r '.access_token')
+
+echo Fetch site id
+response_site=$(curl -s -X GET \
+    "https://graph.microsoft.com/v1.0/sites/$sharepoint_url:/sites/$site_name" \
+    -H "Authorization: Bearer $access_token")
+
+site_id=$(echo $response_site | jq -r '.id')
+
+echo Fetch drive id
+response_drive=$(curl -s -X GET \
+    "https://graph.microsoft.com/v1.0/sites/$site_id/drives" \
+    -H "Authorization: Bearer $access_token")
+
+drive_id="$(echo $response_drive | jq -r '.value[] | select(.name=="Documents") | .id')"
+
+echo Fetch folder id
+response_folder=$(curl -s -X GET \
+    "https://graph.microsoft.com/v1.0/sites/$site_id/drives/$drive_id/root/children" \
+    -H "Authorization: Bearer $access_token")
+
+folder_id=$(echo $response_folder | jq -r '.value[] | select(.name=="General") | .id')
+
+response_item=$(curl -s -X GET \
+    "https://graph.microsoft.com/v1.0/sites/$site_id/drives/$drive_id/items/$folder_id/children" \
+    -H "Authorization: Bearer $access_token")
+
+item_id=$(echo $response_item | jq -r '.value[] | select(.name=="'$file_name'") | .id')
+
+echo Downloading...
+wget --quiet --continue \
+    --header="Authorization: Bearer $access_token" \
+    "https://graph.microsoft.com/v1.0/sites/$site_id/drives/$drive_id/items/$item_id/content" \
+    -O "$destination$file_name"
+
+
